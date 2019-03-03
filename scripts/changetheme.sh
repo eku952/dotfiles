6a72599a65fcb3f6e452555714eea357c15e75bd
eku952 <luke.langefels@comcast.net>
2019-03-03 17:17:00 -0600

    Added scripts

---
+++ scripts/changetheme.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [$# -eq 0 ]
+  then
+    echo "Please specify path to the picture to set as primary background and theme"
+fi
+
+echo Setting wal theme
+wal -i $1 -o "~/.config/scripts/setTermiteConfig.sh"
+
+echo Restoring wallpaper
+feh --bg-fill $1
+
+echo Remember to change the wallpaper settings for feh in ~/.config/i3/config
